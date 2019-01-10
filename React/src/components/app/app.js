91e635c45018ed818646208efff016baa831ceef
Jared Kjar <jared_kjar@crlaurence.com>
2019-01-10 11:26:22 -0800

    added config for nested urls routing

--- React/src/components/app/app.js
+++ React/src/components/app/app.js
@@ -4,9 +4,14 @@ import ReactDefault from '../react-default/react-default';
 import Home from '../home/home';
 import { Container, Row, Col } from 'reactstrap';
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import { myConfig } from '../../config';
 
 class App extends Component {
+
+    
+
     render() {
+        console.log(myConfig.site);
         return (
             <Router>
                 <Container>
@@ -15,8 +20,8 @@ class App extends Component {
                             <Header />
                             <Col>
                                 <div>
-                                    <Route path="/" component={Home} />
-                                    <Route path="/default" component={ReactDefault} />
+                                    <Route path={myConfig.site + '/'} component={Home} />
+                                    <Route path={myConfig.site + '/default'} component={ReactDefault} />
                                 </div>
                             </Col>
                         </Col>
